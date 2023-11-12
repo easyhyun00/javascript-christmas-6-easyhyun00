93c20ad7c8d0b0dbebc8394437499ecc7a3d4a46
easyhyun00 <leejihyun0324@gmail.com>
2023-11-12 14:30:44 +0900

    fix(orderDate): 방문 날짜 입력 예외 메시지 수정
    - 테스트에 통과되도록 예외 처리 메시지 수정

---
+++ src/model/OrderMenu.js
@@ -0,0 +1,52 @@
+import { Console } from "@woowacourse/mission-utils";
+import { ERROR_MESSAGES } from "../utils/Message";
+import RestaurantMenu from "./RestaurantMenu";
+
+class OrderMenu {
+  #menu;
+
+  constructor(menu) {
+    this.menuData = menu;
+    this.orderItems = {};
+    this.#processMenu(menu);
+  }
+
+  #processMenu(menuString) {
+    const menuList = menuString.split(",");
+    menuList.forEach((order) => {
+      const [menuName, menuCount] = order.split("-");
+      this.validate(menuName, Number(menuCount));
+    });
+  }
+
+  validate(menuName, menuCount) {
+    if (
+      !menuName ||
+      Number.isNaN(menuCount) ||
+      menuCount < 1 ||
+      !RestaurantMenu.isMenuExist(menuName) ||
+      this.isOnlyDrink(menuName) ||
+      menuName in this.orderItems ||
+      this.calculateTotalCount() + menuCount > 20
+    ) {
+      throw new Error(ERROR_MESSAGES.주문_메뉴_예외);
+    }
+    this.addOrder(menuName, menuCount);
+  }
+
+  isOnlyDrink(menuName) {
+    const commaCount = (this.menuData.match(/,/g) || []).length;
+    const isDrink = menuName in RestaurantMenu.drink;
+    return commaCount === 0 && isDrink;
+  }
+
+  calculateTotalCount() {
+    return Object.values(this.orderItems).reduce((total, qty) => total + qty, 0);
+  }
+
+  addOrder(menuName, menuCount) {
+    this.orderItems[menuName] = menuCount;
+  }
+}
+
+export default OrderMenu;
--- src/utils/Message.js
+++ src/utils/Message.js
@@ -1,11 +1,13 @@
 const EVENT_MESSAGES = {
   GRETTING: "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
   ORDER_DATE: "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)",
+  ORDER_MENU:
+    "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)",
 };
 
 const ERROR_MESSAGES = {
-  NOT_NUMBER: "[ERROR] 방문 날짜는 숫자여야 합니다.",
-  OUT_OF_DATE: "[ERROR] 방문 날짜는 1일부터 31일 사이여야 합니다.",
+  예약_날짜_예외: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
+  주문_메뉴_예외: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
 };
 
 export { EVENT_MESSAGES, ERROR_MESSAGES };
